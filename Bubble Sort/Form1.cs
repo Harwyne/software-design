8406943da219a161d76e390e5726bb15ecd1ec7c
Harwyne <h.basarte.550409@umindanao.edu.ph>
2025-04-13 09:37:43 +0800

    added BubbleSort and Custom Descending

--- Bubble Sort/Form1.cs
+++ Bubble Sort/Form1.cs
@@ -6,21 +6,38 @@ namespace lab1
         {
             InitializeComponent();
         }
+
         public int[] ProceduralSort(int[] numbers)
         {
-            Array.Sort(numbers);
-            return numbers;
+            int[] sorted = (int[])numbers.Clone();
+            int n = sorted.Length;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                for (int j = 0; j < n - i - 1; j++)
+                {
+                    if (sorted[j] > sorted[j + 1])
+                    {
+                        int temp = sorted[j];
+                        sorted[j] = sorted[j + 1];
+                        sorted[j + 1] = temp;
+                    }
+                }
+            }
+
+            return sorted;
         }
+
         private void btnSort_Click(object sender, EventArgs e)
         {
             int[] numbers = { 3, 1, 5, 2, 4 };
             int[] sortedNumbers = ProceduralSort(numbers);
+            listBoxResults.DataSource = null;
             listBoxResults.DataSource = sortedNumbers;
         }
 
         private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
-
         }
     }
 }
---
+++ Task4/Form1.Designer.cs
@@ -0,0 +1,70 @@
+﻿namespace Task4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnSort = new Button();
+            listBoxResults = new ListBox();
+            SuspendLayout();
+            // 
+            // btnSort
+            // 
+            btnSort.Location = new Point(160, 93);
+            btnSort.Name = "btnSort";
+            btnSort.Size = new Size(94, 29);
+            btnSort.TabIndex = 0;
+            btnSort.Text = "Sort";
+            btnSort.UseVisualStyleBackColor = true;
+            btnSort.Click += btnSort_Click;
+            // 
+            // listBoxResults
+            // 
+            listBoxResults.FormattingEnabled = true;
+            listBoxResults.Location = new Point(380, 113);
+            listBoxResults.Name = "listBoxResults";
+            listBoxResults.Size = new Size(150, 104);
+            listBoxResults.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBoxResults);
+            Controls.Add(btnSort);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnSort;
+        private ListBox listBoxResults;
+    }
+}
