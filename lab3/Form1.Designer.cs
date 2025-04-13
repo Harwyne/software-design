abb468fd36773e34f6baefe7f9b00f15f813e6d2
Harwyne <h.basarte.550409@umindanao.edu.ph>
2025-04-13 11:34:11 +0800

    added inheritance and polymorphism

--- lab3/Form1.Designer.cs
+++ lab3/Form1.Designer.cs
@@ -20,20 +20,52 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            btnShowInfo = new Button();
+            listBox1 = new ListBox();
+            SuspendLayout();
+            // 
+            // btnShowInfo
+            // 
+            btnShowInfo.Location = new Point(140, 70);
+            btnShowInfo.Name = "btnShowInfo";
+            btnShowInfo.Size = new Size(94, 29);
+            btnShowInfo.TabIndex = 0;
+            btnShowInfo.Text = "Show Info";
+            btnShowInfo.UseVisualStyleBackColor = true;
+            btnShowInfo.Click += btnProcessBooks_Click;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(338, 79);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(331, 244);
+            listBox1.TabIndex = 1;
+            listBox1.SelectedIndexChanged += listBox1_SelectedIndexChanged;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(listBox1);
+            Controls.Add(btnShowInfo);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Button btnShowInfo;
+        private ListBox listBox1;
+    }
 }
--- lab3/Form1.cs
+++ lab3/Form1.cs
@@ -1,10 +1,102 @@
 namespace lab3
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public class Book
+        {
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public virtual string GetInfo()
+            {
+                return $"{Title} by {Author}";
+            }
+        }
+
+        public class Magazine : Book
+        {
+            public int IssueNumber { get; set; }
+            public override string GetInfo()
+            {
+                return $"{Title} - Issue {IssueNumber}";
+
+            }
+        }
+        private void btnShowInfo_Click(object sender, EventArgs e)
+        {
+            Book myBook = new Magazine
+            {
+                Title = "Teck Weekly",
+                Author = " John Doe",
+                IssueNumber = 45
+            };
+            MessageBox.Show(myBook.GetInfo());
+        }
+        
+        public class Ebook : Book
+        {
+            public string Format { get; set; }
+            public int FileSizeMB { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} - {Format} ({FileSizeMB} MB)";
+            }
+        }
+
+        
+        public void ProcessBooks(Book[] books)
+        {
+            listBox1.Items.Clear();
+
+            foreach (Book book in books)
+            {
+                
+                string info = book.GetInfo();
+
+                
+                if (book is Magazine)
+                    listBox1.Items.Add($"MAGAZINE: {info}");
+                else if (book is Ebook)
+                    listBox1.Items.Add($"E-BOOK: {info}");
+                else if (book is AudioBook)
+                    listBox1.Items.Add($"AUDIO BOOK: {info}");
+                else
+                    listBox1.Items.Add($"BOOK: {info}");
+            }
+        }
+
+        
+        private void btnProcessBooks_Click(object sender, EventArgs e)
+        {
+            Book[] bookCollection = new Book[]
+    {
+        new Book { Title = "The Great Novel", Author = "Jane Smith" },
+        new Magazine { Title = "Tech Weekly", Author = "John Doe", IssueNumber = 45 },
+        new Ebook { Title = "Digital Design", Author = "Alan Johnson", Format = "PDF", FileSizeMB = 12 },
+        new AudioBook { Title = "Learning C#", Author = "Mark Wilson", Narrator = "Sarah Johnson", DurationHours = 8.5 },
+        new AudioBook { Title = "Adventure Stories", Author = "Emily Brown", Narrator = "Michael Davis", DurationHours = 12.3 }
+    };
+
+            ProcessBooks(bookCollection);
+        }
+        public class AudioBook : Book
+        {
+            public double DurationHours { get; set; }
+            public string Narrator { get; set; }
+
+            public override string GetInfo()
+            {
+                return $"{Title} by {Author} - Narrated by {Narrator} ({DurationHours:F1} hours)";
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
