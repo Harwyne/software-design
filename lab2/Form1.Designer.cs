fda0515f8e8a3e1a1edea7b33ef01bc674d80f95
Harwyne <h.basarte.550409@umindanao.edu.ph>
2025-04-13 10:56:33 +0800

    added Calculate Fibonacci and Power

--- lab2/Form1.Designer.cs
+++ lab2/Form1.Designer.cs
@@ -20,20 +20,178 @@
 			base.Dispose(disposing);
 		}
 
-		#region Windows Form Designer generated code
+        #region Windows Form Designer generated code
 
-		/// <summary>
-		///  Required method for Designer support - do not modify
-		///  the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
-			this.components = new System.ComponentModel.Container();
-			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Text = "Form1";
-		}
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblResult = new Label();
+            txtInput = new TextBox();
+            btnCalculateFactorial = new Button();
+            txtArrayInput = new TextBox();
+            btnCalculateSum = new Button();
+            lblSumResult = new Label();
+            lblFibonacciResult = new Label();
+            btnCalculateFibonacci = new Button();
+            txtInpArray = new TextBox();
+            lblPowerResult = new Label();
+            btnCalculatePower = new Button();
+            txtBaseInput = new TextBox();
+            txtExponentInput = new TextBox();
+            SuspendLayout();
+            // 
+            // lblResult
+            // 
+            lblResult.AutoSize = true;
+            lblResult.Location = new Point(415, 72);
+            lblResult.Name = "lblResult";
+            lblResult.Size = new Size(49, 20);
+            lblResult.TabIndex = 0;
+            lblResult.Text = "Result";
+            // 
+            // txtInput
+            // 
+            txtInput.Location = new Point(273, 69);
+            txtInput.Name = "txtInput";
+            txtInput.Size = new Size(125, 27);
+            txtInput.TabIndex = 1;
+            // 
+            // btnCalculateFactorial
+            // 
+            btnCalculateFactorial.Location = new Point(78, 68);
+            btnCalculateFactorial.Name = "btnCalculateFactorial";
+            btnCalculateFactorial.Size = new Size(189, 29);
+            btnCalculateFactorial.TabIndex = 2;
+            btnCalculateFactorial.Text = "Calculate Factorial";
+            btnCalculateFactorial.UseVisualStyleBackColor = true;
+            btnCalculateFactorial.Click += btnCalculateFactorial_Click;
+            // 
+            // txtArrayInput
+            // 
+            txtArrayInput.Location = new Point(271, 136);
+            txtArrayInput.Name = "txtArrayInput";
+            txtArrayInput.Size = new Size(125, 27);
+            txtArrayInput.TabIndex = 3;
+            // 
+            // btnCalculateSum
+            // 
+            btnCalculateSum.Location = new Point(78, 136);
+            btnCalculateSum.Name = "btnCalculateSum";
+            btnCalculateSum.Size = new Size(187, 29);
+            btnCalculateSum.TabIndex = 4;
+            btnCalculateSum.Text = "Calculate Sum";
+            btnCalculateSum.UseVisualStyleBackColor = true;
+            btnCalculateSum.Click += btnCalculateSum_Click;
+            // 
+            // lblSumResult
+            // 
+            lblSumResult.AutoSize = true;
+            lblSumResult.Location = new Point(415, 145);
+            lblSumResult.Name = "lblSumResult";
+            lblSumResult.Size = new Size(49, 20);
+            lblSumResult.TabIndex = 5;
+            lblSumResult.Text = "Result";
+            // 
+            // lblFibonacciResult
+            // 
+            lblFibonacciResult.AutoSize = true;
+            lblFibonacciResult.Location = new Point(417, 202);
+            lblFibonacciResult.Name = "lblFibonacciResult";
+            lblFibonacciResult.Size = new Size(49, 20);
+            lblFibonacciResult.TabIndex = 8;
+            lblFibonacciResult.Text = "Result";
+            // 
+            // btnCalculateFibonacci
+            // 
+            btnCalculateFibonacci.Location = new Point(80, 193);
+            btnCalculateFibonacci.Name = "btnCalculateFibonacci";
+            btnCalculateFibonacci.Size = new Size(187, 29);
+            btnCalculateFibonacci.TabIndex = 7;
+            btnCalculateFibonacci.Text = "Calculate Fibonacci";
+            btnCalculateFibonacci.UseVisualStyleBackColor = true;
+            btnCalculateFibonacci.Click += btnCalculateFibonacci_Click;
+            // 
+            // txtInpArray
+            // 
+            txtInpArray.Location = new Point(273, 193);
+            txtInpArray.Name = "txtInpArray";
+            txtInpArray.Size = new Size(125, 27);
+            txtInpArray.TabIndex = 6;
+            // 
+            // lblPowerResult
+            // 
+            lblPowerResult.AutoSize = true;
+            lblPowerResult.Location = new Point(415, 256);
+            lblPowerResult.Name = "lblPowerResult";
+            lblPowerResult.Size = new Size(49, 20);
+            lblPowerResult.TabIndex = 11;
+            lblPowerResult.Text = "Result";
+            // 
+            // btnCalculatePower
+            // 
+            btnCalculatePower.Location = new Point(78, 247);
+            btnCalculatePower.Name = "btnCalculatePower";
+            btnCalculatePower.Size = new Size(187, 29);
+            btnCalculatePower.TabIndex = 10;
+            btnCalculatePower.Text = "Calculate Power";
+            btnCalculatePower.UseVisualStyleBackColor = true;
+            btnCalculatePower.Click += btnCalculatePower_Click;
+            // 
+            // txtBaseInput
+            // 
+            txtBaseInput.Location = new Point(271, 247);
+            txtBaseInput.Name = "txtBaseInput";
+            txtBaseInput.Size = new Size(125, 27);
+            txtBaseInput.TabIndex = 9;
+            // 
+            // txtExponentInput
+            // 
+            txtExponentInput.Location = new Point(271, 282);
+            txtExponentInput.Name = "txtExponentInput";
+            txtExponentInput.Size = new Size(125, 27);
+            txtExponentInput.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(txtExponentInput);
+            Controls.Add(lblPowerResult);
+            Controls.Add(btnCalculatePower);
+            Controls.Add(txtBaseInput);
+            Controls.Add(lblFibonacciResult);
+            Controls.Add(btnCalculateFibonacci);
+            Controls.Add(txtInpArray);
+            Controls.Add(lblSumResult);
+            Controls.Add(btnCalculateSum);
+            Controls.Add(txtArrayInput);
+            Controls.Add(btnCalculateFactorial);
+            Controls.Add(txtInput);
+            Controls.Add(lblResult);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
 
-		#endregion
-	}
+        #endregion
+
+        private Label lblResult;
+        private TextBox txtInput;
+        private Button btnCalculateFactorial;
+        private TextBox txtArrayInput;
+        private Button btnCalculateSum;
+        private Label lblSumResult;
+        private Label lblFibonacciResult;
+        private Button btnCalculateFibonacci;
+        private TextBox txtInpArray;
+        private Label lblPowerResult;
+        private Button btnCalculatePower;
+        private TextBox txtBaseInput;
+        private TextBox txtExponentInput;
+    }
 }
--- lab2/Form1.cs
+++ lab2/Form1.cs
@@ -1,10 +1,67 @@
 namespace lab2
 {
-	public partial class Form1 : Form
-	{
-		public Form1()
-		{
-			InitializeComponent();
-		}
-	}
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public int RecursiveFactorial(int n)
+        {
+            if (n == 0) return 1;
+            return n * RecursiveFactorial(n - 1);
+        }
+        private void btnCalculateFactorial_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtInput.Text);
+            int result = RecursiveFactorial((int)number);
+            lblResult.Text = $"Factorial: {result}";
+        }
+        public int RecursiveSum(int[] numbers, int n)
+        {
+            if (n == 0) return numbers[0];
+            return numbers[n] + RecursiveSum(numbers, n - 1);
+        }
+
+        private void btnCalculateSum_Click(object sender, EventArgs e)
+        {
+            int[] numbers = txtArrayInput.Text.Split(',').Select(int.Parse).ToArray();
+            int result = RecursiveSum(numbers, numbers.Length - 1);
+            lblSumResult.Text = $"Sum : {result}";
+
+        }
+        public int RecursiveFibonacci(int n)
+        {
+            
+            if (n <= 0) return 0;
+            if (n == 1) return 1;
+            
+            return RecursiveFibonacci(n - 1) + RecursiveFibonacci(n - 2);
+        }   
+
+        private void btnCalculateFibonacci_Click(object sender, EventArgs e)
+        {
+            int number = int.Parse(txtInpArray.Text);
+            int result = RecursiveFibonacci(number);
+            lblFibonacciResult.Text = $"Fibonacci: {result}";
+        }
+        public double RecursivePower(double x, int n)
+        {
+            
+            if (n == 0) return 1;
+            if (n < 0) return 1 / RecursivePower(x, -n);
+
+            
+            return x * RecursivePower(x, n - 1);
+        }
+        private void btnCalculatePower_Click(object sender, EventArgs e)
+        {
+            double baseValue = double.Parse(txtBaseInput.Text);
+            int exponent = int.Parse(txtExponentInput.Text);
+
+            double result = RecursivePower(baseValue, exponent);
+            lblPowerResult.Text = $"{baseValue}^{exponent} = {result}";
+        }
+
+    }
 }
